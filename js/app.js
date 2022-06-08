6204c74ffb84f08dc041a212644155a26d0dc20e
Eklas-me <kh.Eklas502@gmail.com>
2022-06-08 23:37:44 +0600

    third

--- js/app.js
+++ js/app.js
@@ -28,8 +28,73 @@ exit.onclick = () => {
 start.onclick = () => {
   question.classList.add("active");
   openRuleBox.classList.remove("active");
+  showQuestions(0);
   
+
 }
+
+let nextbtn = document.querySelector(".nextbtn");
+let quesCount = 0;
+
+nextbtn.onclick = () => { 
+
+  if (quesCount < questions.length - 1) {
+    quesCount++;
+    showQuestions(quesCount);
+  }else{
+    alert("Quiz Over");
+  }
+}
+
+
+
+// create function to display questions
+
+function showQuestions(index) {
+  let quesText = document.querySelector(".myquestion");
+  let quesTag = "<span>" + questions[index].numb + `.` + questions[index].question+ "</span>";
+  quesText.innerHTML = quesTag;
+  let options = document.querySelector(".myoptions");
+  let optionsTag = `<div class="options"><span>`+ questions[index].options[0] +`</span></div>`;
+  optionsTag += `<div class="options"><span>`+ questions[index].options[1] +`</span></div>`;
+  optionsTag += `<div class="options"><span>`+ questions[index].options[2] +`</span></div>`;
+  optionsTag += `<div class="options"><span>`+ questions[index].options[3] +`</span></div>`;
+  options.innerHTML = optionsTag;
+  
+  let questionCount = document.querySelector(".question_left"); 
+  let questionCountTag = `<span>`+ questions[index].numb + ` Of ` + questions.length + ` Questions` + `</span>`;
+  questionCount.innerHTML = questionCountTag;
+
+  let option = document.querySelectorAll(".options");
+  for (let i = 0; i < option.length; i++) { 
+    option[0].setAttribute("onclick", "checkAnswer(this)");
+    option[1].setAttribute("onclick", "checkAnswer(this)");
+    option[2].setAttribute("onclick", "checkAnswer(this)");
+    option[3].setAttribute("onclick", "checkAnswer(this)");
+} 
+
+};
+
+
+function checkAnswer(answer){
+  let  userAnswer = answer.textContent;
+  let correctAnswer = questions[quesCount].answer;
+  if (userAnswer === correctAnswer) {
+    answer.classList.add("correct");
+  } else {
+
+    answer.classList.add("wrong");
+  }
+
+}
+
+
+
+
+
+
+
+
 
 
 
---
+++ js/questions.js
@@ -0,0 +1,69 @@
+let questions = [
+
+  {
+    numb: 1,
+    question: "What Dose Html Stand For?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyperlinks and Text Markup Language",
+      "Home Tool Markup Language",
+      "Hyper Text Markup Language",
+      "Hyper Text Meetup Language",
+    ]
+  },
+  {
+    numb: 2,
+    question: "What Dose Css Stand For?",
+    answer: "Cascading Style Sheets",
+    options: [
+      "Colorful Style Sheets",
+      "Cascading Style Sheets",
+      "Computer Style Sheets",
+      "Creative Style Sheets",
+    ]
+  },
+  {
+    numb: 3,
+    question: "What Dose Php Stand For?",
+    answer: "PHP: Hypertext Preprocessor",
+    options: [
+      "PHP: Hypertext Preprocessor",
+      "Personal Hypertext Processor",
+      "Peragraph Home Page",
+      "Private Home Page",
+    ]
+  },
+  {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Structured Query Language",
+      "Structured Question Language",
+      "Strong Question Language",
+      "A programming language",
+    ]
+  },
+  {
+    numb: 5,
+    question: `How do you write "Hello World" in an alert box?`,
+    answer: `alert("Hello World")`,
+    options: [
+      `alertBox("Hello World")`,
+      `msgBox("Hello World")`,
+      `msg("Hello World")`,
+      `alert("Hello World")`,
+    ]
+  },
+  {
+    numb: 6,
+    question: "Who is making the Web standards?",
+    answer: "The World Wide Web Consortium",
+    options: [
+      "Google",
+      "Mozilla",
+      "The World Wide Web Consortium",
+      "Microsoft",
+    ]
+  },
+]
