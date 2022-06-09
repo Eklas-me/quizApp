b850c80b454bdf5590e1fd7e7f76e4c6e74516f8
Eklas-me <kh.Eklas502@gmail.com>
2022-06-09 20:43:51 +0600

    next

--- js/app.js
+++ js/app.js
@@ -29,6 +29,8 @@ start.onclick = () => {
   question.classList.add("active");
   openRuleBox.classList.remove("active");
   showQuestions(0);
+  startTimer(15); 
+  startrTimerLine(0);
   
 
 }
@@ -41,6 +43,12 @@ nextbtn.onclick = () => {
   if (quesCount < questions.length - 1) {
     quesCount++;
     showQuestions(quesCount);
+    clearInterval(counter);
+    startTimer(timeValue); 
+    clearInterval(counterLine);
+    startrTimerLine(widthValue);
+    nextbtn.style.display = "none";
+
   }else{
     alert("Quiz Over");
   }
@@ -52,19 +60,24 @@ nextbtn.onclick = () => {
 
 function showQuestions(index) {
   let quesText = document.querySelector(".myquestion");
+  let optionsList = document.querySelector(".myoptions");
+  let optionsTag = `<div class="options"><span>`+ questions[index].options[0] +`</span></div>`
+                    + `<div class="options"><span>`+ questions[index].options[1] +`</span></div>` 
+                    + `<div class="options"><span>`+ questions[index].options[2] +`</span></div>`
+                    + `<div class="options"><span>`+ questions[index].options[3] +`</span></div>`
   let quesTag = "<span>" + questions[index].numb + `.` + questions[index].question+ "</span>";
   quesText.innerHTML = quesTag;
-  let options = document.querySelector(".myoptions");
-  let optionsTag = `<div class="options"><span>`+ questions[index].options[0] +`</span></div>`;
-  optionsTag += `<div class="options"><span>`+ questions[index].options[1] +`</span></div>`;
-  optionsTag += `<div class="options"><span>`+ questions[index].options[2] +`</span></div>`;
-  optionsTag += `<div class="options"><span>`+ questions[index].options[3] +`</span></div>`;
-  options.innerHTML = optionsTag;
+  optionsList.innerHTML = optionsTag;
+
+
+  // optionsTag += `<div class="options"><span>`+ questions[index].options[1] +`</span></div>`;
+  // optionsTag += `<div class="options"><span>`+ questions[index].options[2] +`</span></div>`;
+  // optionsTag += `<div class="options"><span>`+ questions[index].options[3] +`</span></div>`;
+  // options.innerHTML = optionsTag;
   
   let questionCount = document.querySelector(".question_left"); 
   let questionCountTag = `<span>`+ questions[index].numb + ` Of ` + questions.length + ` Questions` + `</span>`;
   questionCount.innerHTML = questionCountTag;
-
   let option = document.querySelectorAll(".options");
   for (let i = 0; i < option.length; i++) { 
     option[0].setAttribute("onclick", "checkAnswer(this)");
@@ -73,19 +86,78 @@ function showQuestions(index) {
     option[3].setAttribute("onclick", "checkAnswer(this)");
 } 
 
-};
+}
 
+
+let tickIcon = `<div class="tickicon"><i class="bi bi-check-circle-fill"></i></div>`
+let crossIcon = `<div class="crossicon"><i class="bi bi-x-circle-fill"></i> </div>`
 
 function checkAnswer(answer){
+  clearInterval(counter);
+  clearInterval(counterLine);
   let  userAnswer = answer.textContent;
   let correctAnswer = questions[quesCount].answer;
+  let desabledOptions = document.querySelector(".myoptions");
+  let alloption = desabledOptions.children.length;
   if (userAnswer === correctAnswer) {
     answer.classList.add("correct");
+    answer.insertAdjacentHTML("beforeend", tickIcon);
   } else {
-
+    answer.insertAdjacentHTML("beforeend", crossIcon);
     answer.classList.add("wrong");
+
+    for (let i = 0; i < alloption; i++) {
+      if (desabledOptions.children[i].textContent === correctAnswer) {
+        desabledOptions.children[i].classList.add("correct");
+        desabledOptions.children[i].insertAdjacentHTML("beforeend", tickIcon);
+      }
+    }
   }
 
+  for (let i = 0; i < alloption; i++) {
+    desabledOptions.children[i].classList.add("disabled");
+  }
+  nextbtn.style.display = "block";
+ 
+  
+}
+
+let timeCount = document.querySelector(".secends span");
+let timeValue = 15;  
+let counter; 
+
+function startTimer(time){
+  counter = setInterval(timer, 1000);
+  function timer(){
+    time--;
+    timeCount.textContent = time;
+    if (time < 10) {
+      let addZero =  timeCount.textContent;
+      timeCount.textContent = "0" + addZero;
+    }
+    if (time === 0) {
+      clearInterval(counter);
+      alert("Time Up");
+    } 
+
+  } 
+   
+}
+
+let timeLine = document.querySelector(".timelaps");
+let counterLine;
+let widthValue = 0;
+function startrTimerLine(time){
+  counterLine = setInterval(timer, 150);
+  function timer(){
+    time += 1;  
+    timeLine.style.width = time + "%";
+    if (time > 319) {
+      clearInterval(counterLine);
+    }
+
+  } 
+   
 }
 
 
